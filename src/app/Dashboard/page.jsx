656f70263693efb8d4b57630c62f87647a0a1501
Arlex99 <77251212+Arlex99@users.noreply.github.com>
2023-05-05 01:06:07 -0500

    FEAT: CREATE DASHBOARD PAGE

---
+++ src/app/Dashboard/page.jsx
@@ -0,0 +1,10 @@
+export default function Dashboard(params) {
+    return (
+        <div>
+            <div className="container bg-xd text-center m-5 grid grid-cols-5 text-xd">
+                <p>ola</p>
+            </div>
+        </div>
+    )    
+
+}
